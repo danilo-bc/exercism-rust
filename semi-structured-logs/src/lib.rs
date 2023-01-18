8bf22ae056b51bc5f7ee01f08732d23796b44970
Danilo Cavalcanti <danilobcaval@gmail.com>
2023-01-18 10:49:08 -0300

    Solution to semi-structured-logs problem

--- semi-structured-logs/src/lib.rs
+++ semi-structured-logs/src/lib.rs
@@ -1,24 +1,29 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 /// various log levels
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum LogLevel {
     Info,
     Warning,
     Error,
+    Debug,
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!("return a message for the given log level")
+    match level{
+        LogLevel::Info => return info(message),
+        LogLevel::Warning => return warn(message),
+        LogLevel::Error => return error(message),
+        LogLevel::Debug => return debug(message),
+    }
 }
 pub fn info(message: &str) -> String {
-    unimplemented!("return a message for info log level")
+    "[INFO]: ".to_owned() + message
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!("return a message for warn log level")
+    "[WARNING]: ".to_owned() + message
 }
 pub fn error(message: &str) -> String {
-    unimplemented!("return a message for error log level")
+    "[ERROR]: ".to_owned() + message
+}
+pub fn debug(message: &str) -> String {
+    "[DEBUG]: ".to_owned() + message
 }
