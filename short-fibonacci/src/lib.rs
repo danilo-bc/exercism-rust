55a57342fda03844c8a9a94a36df62ba05728e8c
Danilo Cavalcanti <danilobcaval@gmail.com>
2023-01-18 11:10:51 -0300

    Solution to short-fibonacci problem

--- short-fibonacci/src/lib.rs
+++ short-fibonacci/src/lib.rs
@@ -1,13 +1,13 @@
 /// Create an empty vector
 pub fn create_empty() -> Vec<u8> {
-    unimplemented!()
+    vec![0 as u8;0]
 }
 
 /// Create a buffer of `count` zeroes.
 ///
 /// Applications often use buffers when serializing data to send over the network.
 pub fn create_buffer(count: usize) -> Vec<u8> {
-    unimplemented!("create a zeroized buffer of {} bytes", count)
+    vec![0 as u8;count]
 }
 
 /// Create a vector containing the first five elements of the Fibonacci sequence.
@@ -15,5 +15,12 @@ pub fn create_buffer(count: usize) -> Vec<u8> {
 /// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
 /// Its first five elements are `1, 1, 2, 3, 5`.
 pub fn fibonacci() -> Vec<u8> {
-    unimplemented!()
+    let mut fib_buf = create_buffer(5);
+    for i in 0..=1{
+        fib_buf[i] = 1;
+    }
+    for i in 2..5{
+        fib_buf[i] = fib_buf[i-2] + fib_buf[i-1];
+    }
+    fib_buf
 }
