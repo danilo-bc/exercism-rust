bedffaca77b0f81faf37ccbad5f9c60989991f92
Danilo Cavalcanti <danilobcaval@gmail.com>
2023-01-18 08:55:50 -0300

    Solution for the assembly-line problem

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -1,11 +1,14 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let succ_rate;
+    match speed{
+        1..=4 => succ_rate = 1.0,
+        5..=8 => succ_rate = 0.9,
+        9..=10 => succ_rate = 0.77,
+        _ => succ_rate = 0.0,
+    }
+    (speed as f64) * 221.0 * succ_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60.0) as u32
 }
